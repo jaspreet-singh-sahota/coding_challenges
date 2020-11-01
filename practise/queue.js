0a3f9d7379648338ec2b4e6b67c602e5cef0b1cd
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-11-01 20:08:21 +0530

    Add enqueue method to queue

--- practise/queue.js
+++ practise/queue.js
@@ -15,6 +15,19 @@ class Queue {
     return this.first
   }
 
+  enqueue(value) {
+    const newNode = new Node(value)
+    if (this.length === 0 ) {
+      this.first = newNode
+      this.last = newNode
+    } else {
+      this.last.next = newNode
+      this.last = newNode
+    }
+    this.length +=  1
+    return this
+  }
+
   
 }
 
